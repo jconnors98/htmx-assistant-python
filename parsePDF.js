950872bd3a6a9f5958d2cead47e935c468ab669d
Sue Connors <sue.connors@bccassn.com>
2025-08-06 10:03:07 -0700

    Deploying to Render

--- parsePDF.js
+++ parsePDF.js
@@ -1,6 +1,6 @@
 // parsePDF.js
 import fs from "fs";
-import pdf from "pdf-parse";
+import pdfParse from 'pdf-parse/lib/pdf-parse.js';
 
 export async function parseResume(filePath) {
   const buffer = fs.readFileSync(filePath);
